6ff0d8f699ddfa1be9777b5d4ca5f03dfc226b02
Leaves-D <1261931406@qq.com>
2025-12-24 16:22:48 +0800

    Update 2_6.c

--- 2_6.c
+++ 2_6.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
-int main(){
-        int n,a,b;
-        b=1;
-        scanf("%d",&n);
-        for(a=1;a<n;a++){
+
+int pansu(int n){
+        int a,b=1;
+        for(a=2;a<n;a++){
                 if(0==(n%a))
                         b=a;
         }
         if(b==1)
-        printf("yes\n");
+        return 1;
         else
-        printf("no\n");
+        return 0;
+        }
+
+int main(){
+        int i,n;
+        printf("输入正数n，输出1~n的所有素数\n");
+        scanf("%d",&n);
+        for(i=2;i<=n;i++){
+        int k=pansu(i);
+        if(k==1)
+                printf("%d\n",i);
+        }
 }
