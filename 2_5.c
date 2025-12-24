485e099c7b563388ef2591a3619ffbf9313a63cf
Leaves-D <1261931406@qq.com>
2025-12-24 16:20:41 +0800

    Create 2_5.c

---
+++ 2_5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+        int n,a,b;
+        b=0;
+        scanf("%d",&n);
+        for(a=1;a<=n;a++){
+                if(1==(a%2))
+                        b+=a;
+        }
+        printf("%d\n",b);
+}
+~      
